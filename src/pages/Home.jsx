4d185f6451911022be07de76bde24941ab6ffe13
Thuong Ho <thuong.t.ho@gmail.com>
2020-10-22 21:15:56 -0700

    Add some hooks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Chains from '../components/Chains/Chains';
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Chains name='Chains' />
     </div>
   );
 };
